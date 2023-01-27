4096f3ee7f3c65705d26f5ed44c20402800427e8
LandryBulls <bulls409@gmail.com>
2023-01-27 12:29:31 -0500

    default language code to en

--- voicolate/transcribe.py
+++ voicolate/transcribe.py
@@ -12,7 +12,7 @@ model = whisperx.load_model("large", device)
 
 def transcribe(audio):
     # WhisperX handles audio parameter so that it can be a path (str), np.ndarray, or torch.tensor
-    result = model.transcribe(audio)
+    result = model.transcribe(audio, language='en')
     model_a, metadata = whisperx.load_align_model(language_code='en', device=device)
     # align whisper output
     result_aligned = whisperx.align(result["segments"], model_a, metadata, audio, device)
