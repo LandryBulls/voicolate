3222154960a342f92b9e1f1c56e58a11c3d4aaf6
LandryBulls <bulls409@gmail.com>
2023-09-25 16:59:13 -0400

    transcription working :)

--- voicolate/transcribe.py
+++ voicolate/transcribe.py
@@ -10,10 +10,14 @@ else:
 
 model = whisperx.load_model("large", device)
 
-def transcribe(audio):
+def transcribe(audio_file):
     # WhisperX handles audio parameter so that it can be a path (str), np.ndarray, or torch.tensor
+    # But here we're loading an audio file.
+    audio, sr = librosa.load(audio_file, sr=44100)
+    # resample to 16kHz
+    audio = librosa.resample(audio, orig_sr=sr, target_sr=16000)
     result = model.transcribe(audio, language='en')
     model_a, metadata = whisperx.load_align_model(language_code='en', device=device)
     # align whisper output
     result_aligned = whisperx.align(result["segments"], model_a, metadata, audio, device)
-    return result_aligned['segments']
+    return result_aligned
